4920e1cddfb76426b04716cc2b3bf48da93f6225
hiraku <hiraku@localhost>
2007-12-30 21:32:07 +0900

    Add eim_xml/matcher.rb for RSpec

---
+++ lib/eim_xml/matcher.rb
@@ -0,0 +1,25 @@
+require "eim_xml"
+module EimXML::Matchers
+	class HaveContent
+		def initialize(expected)
+			@expected = expected
+		end
+
+		def matches?(target)
+			@target = target
+			@target.has?(@expected)
+		end
+
+		def failure_message
+			"expected #{@target.inspect} must have #{@expected}, but not."
+		end
+
+		def negative_failure_message
+			"expected #{@target.inspect} must not have #{@expected}, but has."
+		end
+	end
+
+	def have(expected)
+		HaveContent.new(expected)
+	end
+end
