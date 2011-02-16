6b75042fcf8127bc52c17e5ee56baf25febeaeb5
KURODA Hiraku <hiraku@hinet.mydns.jp>
2011-02-16 18:15:06 +0900

    Rename assertions_spec.rb to assertions_test.rb, because this file is for test/unit but not rspec.

--- spec/assertions_test.rb
+++ spec/assertions_test.rb
@@ -3,6 +3,7 @@
 # Copyright (C) 2006, KURODA Hiraku <hiraku@hinet.mydns.jp>
 # You can redistribute it and/or modify it under GPL2.
 
+$:.unshift "#{File.dirname(File.dirname(File.expand_path(__FILE__)))}/lib"
 require "test/unit"
 require "eim_xml/assertions"
 
